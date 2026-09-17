0c72bd427030a36b1b22a45cd1aa06e69d20c9db
guilhermegbenvenutti <guilhermegbenvenutti@gmail.com>
2026-09-17 18:01:17 -0300

    Usa RootApp com alternância de visualização

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { App } from "./App";
+import { RootApp } from "./RootApp";
 import "./styles.css";
+import "./excel-view.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RootApp />
   </StrictMode>,
 );
