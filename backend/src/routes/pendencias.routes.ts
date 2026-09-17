b230a647db417291e89d3e7dc98a960ed8821100
guilhermegbenvenutti <guilhermegbenvenutti@gmail.com>
2026-09-17 15:01:31 -0300

    Adiciona rota de mensagem da pendência

--- backend/src/routes/pendencias.routes.ts
+++ backend/src/routes/pendencias.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { gerarMensagemWhatsApp } from "../controllers/mensagens.controller.ts";
 import {
   atualizarPendencia,
   buscarPendenciaPorId,
@@ -12,6 +13,7 @@ export const pendenciasRouter = Router();
 
 pendenciasRouter.get("/", listarPendencias);
 pendenciasRouter.get("/:id/historico", listarHistoricoPendencia);
+pendenciasRouter.get("/:id/mensagem", gerarMensagemWhatsApp);
 pendenciasRouter.get("/:id", buscarPendenciaPorId);
 pendenciasRouter.post("/", criarPendencia);
 pendenciasRouter.patch("/:id", atualizarPendencia);
