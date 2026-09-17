2bec6ee989bae96f2ba5ff1d3323c2aa3421594f
guilhermegbenvenutti <guilhermegbenvenutti@gmail.com>
2026-09-17 14:46:34 -0300

    Adiciona rota de histórico das pendências

--- backend/src/routes/pendencias.routes.ts
+++ backend/src/routes/pendencias.routes.ts
@@ -4,12 +4,14 @@ import {
   atualizarPendencia,
   buscarPendenciaPorId,
   criarPendencia,
+  listarHistoricoPendencia,
   listarPendencias,
 } from "../controllers/pendencias.controller.ts";
 
 export const pendenciasRouter = Router();
 
 pendenciasRouter.get("/", listarPendencias);
+pendenciasRouter.get("/:id/historico", listarHistoricoPendencia);
 pendenciasRouter.get("/:id", buscarPendenciaPorId);
 pendenciasRouter.post("/", criarPendencia);
 pendenciasRouter.patch("/:id", atualizarPendencia);
